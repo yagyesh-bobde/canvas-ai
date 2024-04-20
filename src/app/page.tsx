f777ccf832961edfd95c760365cb356bdaa2427e
yagyesh-bobde <yagyeshsrbobde@gmail.com>
2024-04-20 18:50:49 +0530

    dynamic render and drizzle kit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
+import { headers } from "next/headers";
 import Image from "next/image";
 import Header from "~/components/Header/Header";
+import { db } from "~/server/db";
 
-export default function HomePage() {
 
+export const dynamic = "force-dynamic";
+
+export default async function HomePage() {
+  headers();
   const images = ["https://i.ytimg.com/vi/8NMJxyDwP6A/maxresdefault.jpg", "https://i.ytimg.com/vi/8NMJxyDwP6A/maxresdefault.jpg", "https://i.ytimg.com/vi/8NMJxyDwP6A/maxresdefault.jpg"]
   
+
+  const posts = await db.query.posts.findMany();
   
   return (
     <main className="w-responsive">
@@ -14,6 +21,7 @@ export default function HomePage() {
           <Image src={image} key={index} width={200} height={200} alt="random image" />
         ))}
       </div>
+
     </main>
   );
 }
