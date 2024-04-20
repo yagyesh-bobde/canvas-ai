06fec4b43cf9454d32a3c71fb3a907d80ddb6a53
yagyesh-bobde <yagyeshsrbobde@gmail.com>
2024-04-20 22:50:12 +0530

    updated layout

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,20 +1,8 @@
-import "~/styles/globals.css";
 
-import { Inter } from "next/font/google";
-import { ThemeProvider } from "~/components/theme-provider";
-import { ClerkProvider } from "@clerk/nextjs";
 import Sidebar from "~/components/Sidebar/Sidebar";
 
-const inter = Inter({
-  subsets: ["latin"],
-  variable: "--font-sans",
-});
-
 export const metadata = {
-  title: {
-    default: "Canvas-AI",
-    template: "%s | Canvas-AI",
-  },
+  title: "Dashboard",
   description: "A online art evaluator and marketplace for all art fanatics",
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
