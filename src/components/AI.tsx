9ac44951a2d618d7a528b609f833bc059183d094
yagyesh-bobde <yagyeshsrbobde@gmail.com>
2024-04-21 14:46:45 +0530

    updated ai and gemini

--- src/components/AI.tsx
+++ src/components/AI.tsx
@@ -1,13 +1,28 @@
 "use client";
 import React, { useState, useEffect } from "react";
 import Spline from "@splinetool/react-spline";
+import { textTotext } from "~/utils/gemini";
+import { MessageSquareReply } from "lucide-react";
 
 export default function AI() {
   const [isChatbotOpen, setIsChatbotOpen] = useState(false);
   const [data, setData] = useState('');
+  
   const handleClick = () => {
     setIsChatbotOpen(!isChatbotOpen);
+    setresponse('')
+    setData('')
   };
+
+
+  const handleChatResponse = async () => {
+    const response = await textTotext(data);
+    setresponse(response)
+  }
+
+  const [response, setresponse] = useState("")
+
+
   return(
     <div className="container relative min-h-screen">
         <div className="spline-container fixed bottom-0 right-1 z-10 h-[20%] w-[10%]">
@@ -18,20 +33,20 @@ export default function AI() {
           />
         </div>
         <div
-          className={` fixed h-[50%] w-[20%] bg-white rounded-lg text-black text-2xl font-sans flex justify-center bottom-[18%] right-[3%] shadow-md bg-gradient-to-r from-indigo-500 to-green-700  ${
+          className={` fixed h-[50%] w-[30%] bg-white rounded-lg text-black text-2xl font-sans flex justify-center bottom-[18%] right-[3%] shadow-md bg-gradient-to-r from-indigo-500 to-green-700  ${
             isChatbotOpen ? "" : "hidden"
           }`}
         >
-          <div className="flex flex-col gap-2 font-sans ">
-            Arty
+          <div className="flex flex-col gap-2 font-sans w-[90%]">
+            Jarvis
             <div className="bg-black ml-1.5 h-[75%] w-[95%] rounded-md text-white p-2 pl-2">
               {/* {response} This is the response */}
-              This is the response
+              {response.slice(0,300)}
             </div>
             <div className="flex flex-row gap-2 w-[95%] ml-1.5">
               <input className="w-[82%] rounded-md text-white pl-1 text-md" type="text" value={data} onChange={e => setData(e.target.value)}></input>
-              <button className="bg-cyan-500 w-[24%] rounded-md text-sm" > 
-                Click
+              <button className="bg-cyan-500 w-[24%] rounded-md text-sm" onClick={handleChatResponse} > 
+                <MessageSquareReply />
              </button>
             </div>
           </div>
---
+++ src/utils/gemini.ts
@@ -0,0 +1,18 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { GoogleGenerativeAI } from "@google/generative-ai"
+
+const apiKey = process.env.GEMINI_API_KEY! ;
+
+const genAI = new GoogleGenerativeAI("AIzaSyCs3ewwjg6HfeereBUBrWsqfNnH9_a6NPA");
+
+export const textTotext =async (inp: string) =>{
+  const model = genAI.getGenerativeModel({ model: "gemini-pro"});
+  const result = await model.generateContent(inp);
+  const response =result.response;
+  const text: string = response.text();
+
+//   setresponse(text);
+  return text
+}
